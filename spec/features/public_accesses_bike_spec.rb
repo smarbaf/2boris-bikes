fefef19437b394aa0ad593bc96ef45154b7a5af7
smarbaf <smarbaf@hotmail.com>
2015-06-04 12:29:17 +0100

    Addedand Passed Tests for User Getting a Working Bike

---
+++ spec/features/public_accesses_bike_spec.rb
@@ -0,0 +1,8 @@
+feature 'Member of the public accesses bike' do
+  scenario 'docking station releases a working bike' do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike).to be_working
+  end
+
+end
