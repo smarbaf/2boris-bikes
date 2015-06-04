d5a86252e0fe3c5c56ee58df1f29a1de2ad6a58d
smarbaf <smarbaf@hotmail.com>
2015-06-04 13:05:21 +0100

    Passing Tests for No Available Bikes

--- spec/features/public_accesses_bike_spec.rb
+++ spec/features/public_accesses_bike_spec.rb
@@ -1,8 +1,13 @@
 feature 'Member of the public accesses bike' do
   scenario 'docking station releases a working bike' do
     station = DockingStation.new
+    station.dock Bike.new
     bike = station.release_bike
     expect(bike).to be_working
   end
 
+  scenario 'docking station does not release a bike when there is none available' do
+    station = DockingStation.new
+    expect { station.release_bike }.to raise_error 'No bikes available'
+  end
 end
